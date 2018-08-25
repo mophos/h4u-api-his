dc0cad097b8de50922a6e9f2ebeb9645cb50deb5
Thawatchai Saengduan <thawatchai.sea2@gmail.com>
2018-08-25 13:32:01 +0700

    his_hi.madel.th

--- src/routes/services.ts
+++ src/routes/services.ts
@@ -158,10 +158,8 @@ router.get('/view/:hn/:dateServe/:request_id/:uid', async (req: Request, res: Re
                     const procedure = [];
                     let appointment: any = [];
                     let refer: any = [];
-                    // console.log(v);
-                    const rs_diagnosis = await hisModel.getDiagnosis(db, v.vn);
-                    console.log(v.vn);
 
+                    const rs_diagnosis = await hisModel.getDiagnosis(db, v.vn);
                     if (rs_diagnosis.length) {
                         for (const rg of rs_diagnosis) {
                             const objDiagnosis = {
