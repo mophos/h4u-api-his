f510544f809a201f831673993a4a6436b92cade2
naphattharawat <naphattharawat@gmail.com>
2019-07-23 10:47:07 +0700

    jhos jvkk suansaranrom edit model

--- src/models/his_jvk.model.ts
+++ src/models/his_jvk.model.ts
@@ -16,59 +16,61 @@ export class HisJvkModel {
         // ชื่อสถานพยาบาล
         let data = await db.raw(`SELECT h_code AS provider_code, 
         h_name AS provider_name
-    FROM nano_hospital_code AS hospital
-    WHERE h_code = ${providerCode}`);
+        FROM medrec.nano_hospital_code
+        WHERE h_code = ?`, [providerCode]);
         return data[0];
     }
 
     async getProfile(db: Knex, hn: any) {
-        let data = await db.raw(`SELECT 	hn,
+        let data = await db.raw(`SELECT 	
+        hn,
         pa_people_number AS cid,
-            pa_pre_name AS title_name,
-                pa_name AS first_name,
-                    pa_lastname AS last_name
-    FROM nano_patient
-    WHERE hn = '${hn}'`);
+        pa_pre_name AS title_name,
+        pa_name AS first_name,
+        pa_lastname AS last_name
+        FROM nano_patient
+        WHERE hn = ?`, [hn]);
         return data[0];
     }
 
     async getServices(db: Knex, hn: any, dateServe: any) {
-        let data = await db.raw(`SELECT 	visit.hn,
+        let data = await db.raw(`SELECT 	
+        visit.hn,
         to_char(orders.order_date,'YYYY-MM-DD') AS dateServe,
         orders.order_type AS type,
         itemn.name AS detail,
         items.qty AS frequency
-    FROM medrec.nano_visit AS visit
-    INNER JOIN med.neural_order AS orders ON (visit.id = orders.vn_id)
-    INNER JOIN med.neural_order_item AS items ON (orders.id = items.order_id)
-    INNER JOIN finance.sr_item AS itemn ON (items.item_id = itemn.id)
-    WHERE visit.hn = 'hn: any'
-    AND orders.order_date = '${dateServe}'
-    GROUP BY visit.hn,dateserve,order_type,name,qty`);
+        FROM medrec.nano_visit AS visit
+        INNER JOIN med.neural_order AS orders ON (visit.id = orders.vn_id)
+        INNER JOIN med.neural_order_item AS items ON (orders.id = items.order_id)
+        INNER JOIN finance.sr_item AS itemn ON (items.item_id = itemn.id)
+        WHERE visit.hn = ?
+        AND orders.order_date = ?
+        GROUP BY visit.hn,dateserve,order_type,name,qty`, [hn, dateServe]);
         return data[0];
     }
 
     async getAllergyDetail(db: Knex, hn: any) {
-        let data = await db.raw(` SELECT 	drug_name,
+        let data = await db.raw(` SELECT 	
+        drug_name,
         level_name AS symptom
-    FROM nano_patient_allergic
-    WHERE hn = '${hn}'`);
+        FROM nano_patient_allergic
+        WHERE hn = ?`, hn);
         return data[0];
     }
 
     async getChronic(db: Knex, hn: any) {
-        let data = await db.raw(`SELECT 	icd10.code AS icd_code,
+        let data = await db.raw(`SELECT 	
+        icd10.code AS icd_code,
         icd10.name AS icd_name,
         to_char (diag.date,'YYYY-MM-DD') AS start_date
         FROM neural_order_icd10 AS diag
         INNER JOIN med.neural_icd10 AS icd10 ON (icd10.id = diag.icd10_id)
         INNER JOIN medrec.nano_visit AS visit ON (visit.id = diag.visit_id)
         WHERE diag.priority >= '3'
-        AND visit.hn = '${hn}'
-        GROUP BY icd_code,icd_name,start_date`);
+        AND visit.hn = ?
+        GROUP BY icd_code,icd_name,start_date`, [hn]);
         return data[0];
-        // โรคเรื้อรัง
-        // return [{icd_code:'',icd_name:'',start_date:''}]
     }
 
 
@@ -82,9 +84,9 @@ export class HisJvkModel {
         FROM neural_order_icd10 AS diag
         INNER JOIN med.neural_icd10 AS icd10 ON(icd10.id = diag.icd10_id)
         INNER JOIN medrec.nano_visit AS visit ON(visit.id = diag.visit_id)
-        WHERE visit.hn = '${hn}'
-        AND visit.time_add = '${dateServe}'
-        AND visit.id = '${seq}'`);
+        WHERE visit.hn = ?
+        AND visit.time_add = ?
+        AND visit.id = ?`, [hn, dateServe, seq]);
         return data[0];
     }
 
@@ -94,16 +96,16 @@ export class HisJvkModel {
             hospital_refer AS hcode_to,
             hospital.h_name AS name_to,
             dc_reason_detail AS reason
-        FROM mi_ipd_main AS ipd
-        INNER JOIN medrec.nano_hospital_code AS hospital ON(hospital.h_code = ipd.hospital_refer)
-        WHERE hospital_refer IS NOT NULL AND hospital_refer > '1'
-        AND hn = '${hn}'
-        AND ipd.admit_date = '${dateServe}'
-        AND ipd.vn_id = '${seq}'`);
+            FROM mi_ipd_main AS ipd
+            INNER JOIN medrec.nano_hospital_code AS hospital ON(hospital.h_code = ipd.hospital_refer)
+            WHERE hospital_refer IS NOT NULL AND hospital_refer > '1'
+            AND hn = ?
+            AND ipd.admit_date = ?
+            AND ipd.vn_id = ?`, [hn, dateServe, seq]);
         return data[0];
     }
 
-    async getProcedure(db: Knex, hn: any, dateServe: any, vn: any) {
+    async getProcedure(db: Knex, hn: any, dateServe: any, seq: any) {
         // Procedure
         let data = await db.raw(`SELECT    
             visit.id AS seq,
@@ -124,15 +126,12 @@ export class HisJvkModel {
         AND orders.deleted = 'f'
         AND order_item.type != 'drug'
         AND orders.order_type IN('dental', 'psycho', 'ECT', 'operative')
-        AND visit.hn = '${hn}'
-        AND to_char(orders.order_date, 'YYYY-MM-DD') = '${dateServe}'
-        AND visit.vn = '${vn}'
+        AND visit.hn = ?
+        AND to_char(orders.order_date, 'YYYY-MM-DD') = ?
+        AND visit.vn = ?
         GROUP BY seq, procedure_code, procedure_name, date_serv, time_serv, start_date, start_time, end_date, end_time
-        ORDER BY visit.time_add`);
+        ORDER BY visit.time_add`, [hn, dateServe, seq]);
         return data[0];
-
-
-        // return [{seq:'',procedure_code:'',procedure_name:'',date_serv:'',time_serv:'',start_date:'',start_time:'',end_date:'',end_time:''}];
     }
 
     async getDrugs(db: Knex, hn: any, dateServe: any, seq: any) {
@@ -149,15 +148,11 @@ export class HisJvkModel {
     INNER JOIN med.neural_order_item AS items ON(items.drug_id = drug.id)
     INNER JOIN medrec.nano_visit AS visit ON(visit.id = items.vn_id)
     WHERE items.deleted = 'f'
-    AND  visit.hn = '${hn}'
-    AND visit.id = '${seq}'
-    AND to_char(items.date_add, 'YYYY-MM-DD') = '${dateServe}'
-    GROUP BY drug_name, qty, unit, usage_line1, usage_line2, usage_line3, usage_line4`);
+    AND  visit.hn = ?
+    AND visit.id = ?
+    AND to_char(items.date_add, 'YYYY-MM-DD') = ?
+    GROUP BY drug_name, qty, unit, usage_line1, usage_line2, usage_line3, usage_line4`, [hn, dateServe, seq]);
         return data[0];
-        //ยา
-
-
-        // return [{drug_name:'',qty:'',unit:'',usage_line1:'',usage_line2:'',usage_line3:''}]
     }
 
     async getLabs(db: Knex, hn: any, dateServe: any, seq: any) {
@@ -174,19 +169,16 @@ export class HisJvkModel {
         INNER JOIN lab.neural_lab_value  AS value ON(value.id = result.lab_value_id)
         INNER JOIN medrec.nano_visit AS visit ON(visit.id = record.vn_id)
         WHERE items.type = 'lab' AND items.deleted = 'f'
-        AND visit.hn = '${hn}'
-        AND items.date_add = '${dateServe}'
-        AND visit.id = '${seq}'
-        GROUP BY lab_name, lab_result, standard_result, seq, time_serv, date_serv`);
+        AND visit.hn = ?
+        AND items.date_add = ?
+        AND visit.id = ?
+        GROUP BY lab_name, lab_result, standard_result, seq, time_serv, date_serv`,
+            [hn, dateServe, seq]);
         return data[0];
-        //แล็ป
-
-
-        // return [{lab_name:'',lab_result:'',standard_result:'',seq:'',time_serv:'',date_serv:''}]
     }
 
 
-    async getAppointment(db: Knex, hn: any, dateServ: any, seq: any) {
+    async getAppointment(db: Knex, hn: any, dateServe: any, seq: any) {
         let data = await db.raw(`SELECT 	to_char(appointment_date, 'YYYY-MM-DD') AS date,
             to_char(appointment_time, 'HH24:MI:SS') AS time,
                 components.com_name AS department,
@@ -195,15 +187,11 @@ export class HisJvkModel {
         INNER JOIN medrec.nano_visit AS visit ON(visit.id = appointment.visit_id)
         INNER JOIN jvkk.nano_components AS components ON(components.id = appointment.component_id)
         WHERE appointment.deleted = 'f'
-        AND visit.hn = '${hn}'
-        AND to_char(appointment.created_date, 'YYYY-MM-DD') = '${dateServ}'
-        AND visit.id = '${seq}'
-        GROUP BY date, time, department, detail`);
+        AND visit.hn = ?
+        AND to_char(appointment.created_date, 'YYYY-MM-DD') = ?
+        AND visit.id = ?
+        GROUP BY date, time, department, detail`, [hn, dateServe, seq]);
         return data[0];
-        //นัดหมาย
-
-
-        // return [{date:'',time:'',department:'',detail:''}]
     }
 
     async getVaccine(db: Knex, hn: any) {
@@ -219,13 +207,9 @@ export class HisJvkModel {
         WHERE items.item_id IN(2243, 2895, 3367)
         AND orders.deleted = 'f'
         AND items.deleted = 'f'
-        AND visits.hn = '${hn}'
-        ORDER BY orders.order_date`);
+        AND visits.hn = ?
+        ORDER BY orders.order_date`, [hn]);
         return data[0];
-        //วัคซีน
-
-
-        //return [{date_serv:'',time_serv:'',vaccine_code:'',vaccine_name:''}]]
     }
 
 }
--- src/models/his_suansaranrom.model.ts
+++ src/models/his_suansaranrom.model.ts
@@ -2,58 +2,59 @@ import Knex = require('knex');
 export class HisSuansaranromModel {
 
 	async getHospital(db: Knex, providerCode: any, hn: any) {
-		let data = await db.raw(`SELECT
-									hcode AS provider_code,
-									hname AS provider_name
-								FROM hospital_setting
-								WHERE h_code = ${providerCode}`);
+		let data = await db.raw(`
+		SELECT
+		hcode AS provider_code,
+		hname AS provider_name
+		FROM hospital_setting
+		WHERE h_code = ?`, [providerCode]);
 		return data[0];
 	}
 
 	async getProfile(db: Knex, hn: any) {
 		let data = await db.raw(`SELECT
-										hn,
-										CID AS cid,
-										description AS title_name,
-										FNAME AS first_name,
-										LNAME AS last_name
-										FROM tb_person
-										INNER JOIN ref_prename ON tb_person.PRENAME = ref_prename.CODE
-										WHERE tb_person.HN = '${hn}'`);
+		hn,
+		CID AS cid,
+		description AS title_name,
+		FNAME AS first_name,
+		LNAME AS last_name
+		FROM tb_person
+		INNER JOIN ref_prename ON tb_person.PRENAME = ref_prename.CODE
+		WHERE tb_person.HN = ?`, [hn]);
 		return data[0];
 	}
 
 	async getServices(db: Knex, hn: any, dateServe: any) {
 		let data = await db.raw(`SELECT
-									tb_opd.OPD_ID as seq,
-									tb_opd.DATE_SERV as date_serv
-								FROM
-								tb_opd
-								WHERE tb_opd.HN ='${hn}'
-								AND tb_opd.DATE_SERV = '${dateServe}'`);
+		tb_opd.OPD_ID as seq,
+		tb_opd.DATE_SERV as date_serv
+		FROM
+		tb_opd
+		WHERE tb_opd.HN =?
+		AND tb_opd.DATE_SERV =?`, [hn, dateServe]);
 		return data[0];
 
 	}
 
 	async getAllergyDetail(db: Knex, hn: any) {
 		let data = await db.raw(`SELECT
-									opd_allergy.drug_name as drug_name,
-									opd_allergy.symptom as symptom
-								FROM
-								opd_allergy
-								WHERE opd_allergy.HN = '${hn}'`);
+		opd_allergy.drug_name as drug_name,
+		opd_allergy.symptom as symptom
+		FROM
+		opd_allergy
+		WHERE opd_allergy.HN = ?`, [hn]);
 		return data[0];
 
 	}
 
 	async getChronic(db: Knex, hn: any) {
 		let data = await db.raw(`SELECT
-									tb_opd_fu_chronic.icd_code as icd_code,
-									tb_opd_fu_chronic.icd_name as icd_name,
-									tb_opd_fu_chronic.start_date as start_date
-								FROM
-								tb_opd_fu_chronic
-								WHERE tb_opd_fu_chronic.HN = '${hn}'`);
+		tb_opd_fu_chronic.icd_code as icd_code,
+		tb_opd_fu_chronic.icd_name as icd_name,
+		tb_opd_fu_chronic.start_date as start_date
+		FROM
+		tb_opd_fu_chronic
+		WHERE tb_opd_fu_chronic.HN = ?`, [hn]);
 		return data[0];
 
 	}
@@ -61,109 +62,109 @@ export class HisSuansaranromModel {
 
 	async getDiagnosis(db: Knex, hn: any, dateServe: any) {
 		let data = await db.raw(`SELECT
-									 tb_opd_fu_diag.DIAG_DATE AS dateServe,
-									 tb_opd_fu_diag.DIAG_CODE AS icd_code,
-									 ref_diagcode.DESCRIPTION AS icd_name,
-									 ref_diagtype.DESCRIPTION AS diage_type
-								FROM tb_opd_fu_diag
-								INNER JOIN ref_diagcode ON tb_opd_fu_diag.DIAG_CODE = ref_diagcode.CODE
-								INNER JOIN ref_diagtype ON tb_opd_fu_diag.diag_type = ref_diagtype.CODE
-								WHERE tb_opd_fu_diag.HN ='${hn}'
-								AND tb_opd_fu_diag.DIAG_DATE = '${dateServe}'`);
+		tb_opd_fu_diag.DIAG_DATE AS dateServe,
+		tb_opd_fu_diag.DIAG_CODE AS icd_code,
+		ref_diagcode.DESCRIPTION AS icd_name,
+		ref_diagtype.DESCRIPTION AS diage_type
+		FROM tb_opd_fu_diag
+		INNER JOIN ref_diagcode ON tb_opd_fu_diag.DIAG_CODE = ref_diagcode.CODE
+		INNER JOIN ref_diagtype ON tb_opd_fu_diag.diag_type = ref_diagtype.CODE
+		WHERE tb_opd_fu_diag.HN =?
+		AND tb_opd_fu_diag.DIAG_DATE = ?`, [hn, dateServe]);
 		return data[0];
 	}
 
 	async getRefer(db: Knex, hn: any, dateServe: any, seq: any) {
 		let data = await db.raw(`SELECT
-									tb_refer_send.SEND_DATE as date_serv,
-									TIME(tb_refer_send.MODIFY_DATE) as time_serv,
-									tb_refer_send.RECEIVE_HOSP as to_provider_code,
-									ref_hosp.description as to_provider_name,
-									tb_refer_send.SEND_CAUSE as reason,
-									DATE(tb_refer_send.CREATE_DATE) as start_date
-									FROM
-									tb_refer_send
-								INNER JOIN ref_hosp ON tb_refer_send.RECEIVE_HOSP = ref_hosp.code
-								WHERE tb_refer_send.HN = '${hn}'`);
+		tb_refer_send.SEND_DATE as date_serv,
+		TIME(tb_refer_send.MODIFY_DATE) as time_serv,
+		tb_refer_send.RECEIVE_HOSP as to_provider_code,
+		ref_hosp.description as to_provider_name,
+		tb_refer_send.SEND_CAUSE as reason,
+		DATE(tb_refer_send.CREATE_DATE) as start_date
+		FROM
+		tb_refer_send
+		INNER JOIN ref_hosp ON tb_refer_send.RECEIVE_HOSP = ref_hosp.code
+		WHERE tb_refer_send.HN = `, [hn]);
 		return data[0];
 
 	}
 
 	async getProcedure(db: Knex, hn: any, dateServe: any, vn: any) {
 		let data = await db.raw(`SELECT
-									tb_opd_fu_procedure.ORDER_NO as seq,
-									tb_opd_fu_procedure.PROCEDURE_CODE as procedure_code,
-									ref_procedure.description as procedure_name,
-									tb_opd_fu_procedure.PROCEDURE_DATE as start_date,
-									TIME(tb_opd_fu_procedure.MODIFY_DATE) as start_time
-								FROM
-								tb_opd_fu_procedure
-								INNER JOIN ref_procedure ON tb_opd_fu_procedure.PROCEDURE_CODE = ref_procedure.code
-								WHERE tb_opd_fu_procedure.HN ='${hn}'
-								AND tb_opd_fu_procedure.PROCEDURE_DATE = '${dateServe}'`);
+		tb_opd_fu_procedure.ORDER_NO as seq,
+		tb_opd_fu_procedure.PROCEDURE_CODE as procedure_code,
+		ref_procedure.description as procedure_name,
+		tb_opd_fu_procedure.PROCEDURE_DATE as start_date,
+		TIME(tb_opd_fu_procedure.MODIFY_DATE) as start_time
+		FROM
+		tb_opd_fu_procedure
+		INNER JOIN ref_procedure ON tb_opd_fu_procedure.PROCEDURE_CODE = ref_procedure.code
+		WHERE tb_opd_fu_procedure.HN ='${hn}'
+		AND tb_opd_fu_procedure.PROCEDURE_DATE = `, [dateServe]);
 		return data[0];
 
 	}
 
 	async getDrugs(db: Knex, hn: any, dateServe: any, seq: any) {
 		let data = await db.raw(`SELECT
-									tb_drug_item.DNAME AS drug_name,
-									tb_opd_drug.ISSUE_QTY AS qty,
-									tb_opd_drug.DRUG_ADMIN AS unit,
-									tb_opd_drug.DESC1 AS usage_line1,
-									tb_opd_drug.DESC2 AS usage_line2,
-									tb_opd_drug.DESC3 AS usage_line3
-									FROM
-								tb_opd_drug
-								INNER JOIN tb_drug_item ON tb_opd_drug.DID = tb_drug_item.DID
-								WHERE tb_opd_drug.HN ='${hn}'
-								AND tb_opd_drug.ISSUE_DATE = '${dateServe}'`);
+		tb_drug_item.DNAME AS drug_name,
+		tb_opd_drug.ISSUE_QTY AS qty,
+		tb_opd_drug.DRUG_ADMIN AS unit,
+		tb_opd_drug.DESC1 AS usage_line1,
+		tb_opd_drug.DESC2 AS usage_line2,
+		tb_opd_drug.DESC3 AS usage_line3
+		FROM
+		tb_opd_drug
+		INNER JOIN tb_drug_item ON tb_opd_drug.DID = tb_drug_item.DID
+		WHERE tb_opd_drug.HN =?
+		AND tb_opd_drug.ISSUE_DATE = ?`, [hn, dateServe]);
 		return data[0];
 
 	}
 
 	async getLabs(db: Knex, hn: any, dateServe: any, seq: any) {
 		let data = await db.raw(`SELECT
-									tb_lab.ORDER_NO as seq,
-									tb_lab.REQ_DATE as date_serv,
-									TIME(tb_lab.REQ_TIME) as time_serv,
-									tb_lab_master.LAB_NAME as lab_name,
-									tb_lab.LAB_RESULT as lab_result,
-									tb_lab_master.NORMAL_RANGE_DESC as standard_result
-								FROM tb_lab
-								INNER JOIN tb_lab_master ON tb_lab.LAB_CODE = tb_lab_master.LAB_CODE
-								WHERE tb_lab.HN ='${hn}'
-								AND tb_lab.REQ_DATE = '${dateServe}'`);
+		tb_lab.ORDER_NO as seq,
+		tb_lab.REQ_DATE as date_serv,
+		TIME(tb_lab.REQ_TIME) as time_serv,
+		tb_lab_master.LAB_NAME as lab_name,
+		tb_lab.LAB_RESULT as lab_result,
+		tb_lab_master.NORMAL_RANGE_DESC as standard_result
+		FROM tb_lab
+		INNER JOIN tb_lab_master ON tb_lab.LAB_CODE = tb_lab_master.LAB_CODE
+		WHERE tb_lab.HN =?
+		AND tb_lab.REQ_DATE =?`, [hn, dateServe]);
 		return data[0];
 
 	}
 
 	async getAppointment(db: Knex, hn: any, dateServe: any, seq: any) {
 		let data = await db.raw(`SELECT
-									tb_appointment.ORDER_NO as seq,
-									tb_appointment.APPOINTMENT_DATE as date,
-									TIME(tb_appointment.APPOINTMENT_TIME_FROM) AS time,
-									tb_department.DEPARTMENT_NAME as department,
-									IF(CONCAT(tb_appointment_master.DESCRIPTION,':',tb_appointment.BEFORE_APPOINTMENT) IS NULL,'-',CONCAT(tb_appointment_master.DESCRIPTION,':',tb_appointment.BEFORE_APPOINTMENT)) as detail
-								FROM
-								tb_appointment
-								INNER JOIN tb_department ON tb_appointment.NEXT_DEPARTMENT_ID = tb_department.DEPARTMENT_ID
-								LEFT JOIN tb_appointment_master ON tb_appointment.APPOINTMENT_DETAIL_A1 = tb_appointment_master.CODE
-								WHERE tb_appointment.HN ='${hn}'
-								AND tb_appointment.APPOINTMENT_DATE = '${dateServe}'`);
+		tb_appointment.ORDER_NO as seq,
+		tb_appointment.APPOINTMENT_DATE as date,
+		TIME(tb_appointment.APPOINTMENT_TIME_FROM) AS time,
+		tb_department.DEPARTMENT_NAME as department,
+		IF(CONCAT(tb_appointment_master.DESCRIPTION,':',tb_appointment.BEFORE_APPOINTMENT) IS NULL,'-',CONCAT(tb_appointment_master.DESCRIPTION,':',tb_appointment.BEFORE_APPOINTMENT)) as detail
+		FROM
+		tb_appointment
+		INNER JOIN tb_department ON tb_appointment.NEXT_DEPARTMENT_ID = tb_department.DEPARTMENT_ID
+		LEFT JOIN tb_appointment_master ON tb_appointment.APPOINTMENT_DETAIL_A1 = tb_appointment_master.CODE
+		WHERE tb_appointment.HN =?
+		AND tb_appointment.APPOINTMENT_DATE = ?`, [hn, dateServe]);
 		return data[0];
 
 	}
 
 	async getVaccine(db: Knex, hn: any) {
 		let data = await db.raw(`SELECT
-									tb_opd_fu_vaccine.DATE_SERVE as date_serv,
-									TIME(tb_opd_fu_vaccine.TIME_SERVE) as time_serv,
-									tb_opd_fu_vaccine.vaccine_code as vaccine_code,
-									tb_opd_fu_vaccine.vaccine_name as vaccine_name
-								FROM
-								tb_opd_fu_vaccine
-								WHERE tb_opd_fu_vaccine.HN = '${hn}'`);
+		tb_opd_fu_vaccine.DATE_SERVE as date_serv,
+		TIME(tb_opd_fu_vaccine.TIME_SERVE) as time_serv,
+		tb_opd_fu_vaccine.vaccine_code as vaccine_code,
+		tb_opd_fu_vaccine.vaccine_name as vaccine_name
+		FROM
+		tb_opd_fu_vaccine
+		WHERE tb_opd_fu_vaccine.HN = ?`, [hn]);
 		return data[0];
 
 	}
